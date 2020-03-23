986a3a596ab769c6b91e5d589f67fcbf2da995db
antoinnepy <antoinnepy@gmail.com>
2020-03-22 20:04:14 -0400

    Modify file content.

--- nested_list_sum2.py
+++ nested_list_sum2.py
@@ -1,19 +1,19 @@
-def nested_list_sum(lst):
+def nested_list_sum2(nested_list):
     '''
     Calculate sum of a nested list
-    Input: Nested list (Lists)
+    
+    Input: Nested List (Lists)
     Output: Sum (Integer)
+    
     '''
     
-    if lst == []:
-        return 0
-    if type(lst[0]) is list:
-        lst[0] = nested_list_sum(lst[0])
-    return lst[0] + nested_list_sum(lst[1:])
+    return sum([int(item.strip('[ [] ]')) 
+                for item in str(nested_list).split(',') 
+                if item.strip() != '[]'])
 
 def main():
     sample_list = [[7000,[415,[],321],5],6,21,9,[20,32,4],[],[12, [345,[900,12040]],12],11,[0,[],2400,5],19]
-    list_sum = nested_list_sum(sample_list)
+    list_sum = nested_list_sum2(sample_list)
     print("sample list = {}; \nsum of items in list = {}".format(sample_list, list_sum))
     
 if __name__ == "__main__":
